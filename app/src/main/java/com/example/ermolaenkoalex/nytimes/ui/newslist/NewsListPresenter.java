2e8c05ba8f225bdb14b2a0d0194e46fc0c75c95b
AlexHurray <ermolaenkoalex@mail.ru>
2018-10-25 19:00:27 +0300

    Fix PR comment(part2)

--- app/src/main/java/com/example/ermolaenkoalex/nytimes/ui/newslist/NewsListPresenter.java
+++ app/src/main/java/com/example/ermolaenkoalex/nytimes/ui/newslist/NewsListPresenter.java
@@ -8,6 +8,7 @@ import com.example.ermolaenkoalex.nytimes.model.NewsItem;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -16,13 +17,13 @@ import io.reactivex.schedulers.Schedulers;
 
 public class NewsListPresenter extends ViewModel {
 
-    private final String LOG_TAG = "NewsListVM";
+    private static final String LOG_TAG = "NewsListVM";
 
     private Disposable disposable;
     private List<NewsItem> newsList = new ArrayList<>();
     private NewsListView newsListView;
 
-    public void bind(NewsListView newsListView) {
+    public void bind(@NonNull NewsListView newsListView) {
         this.newsListView = newsListView;
     }
 
@@ -30,26 +31,36 @@ public class NewsListPresenter extends ViewModel {
         newsListView = null;
     }
 
-    public void getNews(boolean force) {
-        if (newsList.isEmpty() || force) {
+    public void getNews(boolean forceReload) {
+        if (newsList.isEmpty() || forceReload) {
             loadData();
         } else {
             newsListView.setData(newsList);
         }
     }
 
-    public void loadData() {
+    private void loadData() {
         dispose();
 
         disposable = Observable.fromCallable(DataUtils::generateNews)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe(disposable -> newsListView.showLoading())
-                .doFinally(newsListView::hideLoading)
+                .doOnSubscribe(disposable -> {
+                    if (newsListView != null) {
+                        newsListView.showLoading();
+                    }
+                })
+                .doFinally(() -> {
+                    if (newsListView != null) {
+                        newsListView.hideLoading();
+                    }
+                })
                 .subscribe(newsItems -> {
                     Log.d(LOG_TAG, "subscribe call in " + Thread.currentThread());
 
-                    newsListView.setData(newsItems);
+                    if (newsListView != null) {
+                        newsListView.setData(newsItems);
+                    }
                     newsList.clear();
                     newsList.addAll(newsItems);
                 });
@@ -62,7 +73,7 @@ public class NewsListPresenter extends ViewModel {
     }
 
     private void dispose() {
-        if (disposable != null && !disposable.isDisposed()) {
+        if (disposable != null) {
             disposable.dispose();
         }
     }
