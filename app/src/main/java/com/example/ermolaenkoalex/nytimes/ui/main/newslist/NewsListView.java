5478a6cd6e0ff3103d3abe0283d72d41e3e69c67
AlexHurray <ermolaenkoalex@mail.ru>
2018-12-19 18:30:33 +0300

    Move strategy to interface

--- app/src/main/java/com/example/ermolaenkoalex/nytimes/ui/main/newslist/NewsListView.java
+++ app/src/main/java/com/example/ermolaenkoalex/nytimes/ui/main/newslist/NewsListView.java
@@ -9,13 +9,11 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 
+@StateStrategyType(value = SingleStateStrategy.class)
 public interface NewsListView extends MvpView {
-    @StateStrategyType(value = SingleStateStrategy.class)
     void showRefresher(boolean show);
 
-    @StateStrategyType(value = SingleStateStrategy.class)
     void showData(List<NewsItem> data);
 
-    @StateStrategyType(value = SingleStateStrategy.class)
     void showError(int errorMessageId, boolean isToast);
 }
